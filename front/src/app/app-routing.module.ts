3b519f4ec75063b1b76abc6e3fe56d54249a017a
Nolwenn123 <nolwenn.bourey@gmail.com>
2024-01-31 12:29:27 +0100

    add navbar and remove accueil

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
-  {path: "home", component: AccueilComponent}
 ];
 
 @NgModule({
